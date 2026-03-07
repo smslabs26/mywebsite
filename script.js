20ca171d10bb701aceef72ce6919863b3a327045
smslabs26 <smslabs26@gmail.com>
2026-03-07 15:55:50 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Website load aana message
-window.onload = function() {
-    console.log("Website loaded successfully!");
-}
-
-// Welcome alert
-function welcomeMessage(){
-    alert("Welcome to My Business Website!");
-}
