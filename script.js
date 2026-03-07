cadb89e171182711a6a95d92ea040aea3019bed5
smslabs26 <smslabs26@gmail.com>
2026-03-07 15:56:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Website load aana message
+window.onload = function() {
+    console.log("Website loaded successfully!");
+}
+
+// Welcome alert
+function welcomeMessage(){
+    alert("Welcome to My Business Website!");
+}
